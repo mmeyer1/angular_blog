1b6f996f2ae9845f2cb9ce92ac4680bff2ad9c10
mmeyer1 <matt0719@gmail.com>
2014-06-19 22:54:05 -0400

    Playing with multiple controllers/partials

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 This is a test app based on the Angular Seed. I will be using it to build a
-little Angular JS blog for myself to learn the framework and to experiment. 
+little Angular JS blog for myself to learn the framework and to experiment.
+
+Played around a little with partials and binding between two services, interesting stuff.
+
+I think I may make a new app for assisting making wargames lists, no persistency and
+a very clearly defined objective (I think Angular = optimal for this)
+
+Definitely going to play with Angular on Rails soon (since I know and love Rails!)
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,9 @@
 /* Controllers */
 
 angular.module('angularBlog.controllers', [])
+.factory('EntryFactory', function(){
+  return{message: "Wooo Service!"}
+})
   .controller('MyCtrl1', ['$scope', function($scope) {
 
   }])
@@ -15,4 +18,11 @@ angular.module('angularBlog.controllers', [])
     'stuff': 'Blah BLAH blah blah!'},
     {'title': 'some other post',
     'stuff': 'More BlAH Blah Blah!'}];
-  }]);
+  }])
+.controller('newEntriesCtrl', ['$scope', function($scope){
+  $scope.master = {};
+  $scope.update = function(entry){
+    $scope.master = angular.copy(entry);
+  };
+
+}]);
